bda36dace734427793fb16b112ed3413fc118cb4
OrgOrbit <151468260+purpuscode@users.noreply.github.com>
2026-06-04 05:17:12 +0530

    fix: add dist/index.js as CJS main entry

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.BELONGITY_CHAT_SEND_TASK_ID = void 0;
+var chat_send_1 = require("./chat-send");
+Object.defineProperty(exports, "BELONGITY_CHAT_SEND_TASK_ID", {
+    enumerable: true,
+    get: function () { return chat_send_1.BELONGITY_CHAT_SEND_TASK_ID; }
+});
