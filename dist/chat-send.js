6fb41b1effc7b592635c3bc994abff86fbc61be2
OrgOrbit <151468260+purpuscode@users.noreply.github.com>
2026-06-04 05:17:11 +0530

    fix: add compiled dist/ for CommonJS runtime compatibility

---
+++ dist/chat-send.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.BELONGITY_CHAT_SEND_TASK_ID = void 0;
+exports.BELONGITY_CHAT_SEND_TASK_ID = 'belongity-chat-send';
